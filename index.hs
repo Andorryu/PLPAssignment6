4b5ab0f26ec1324e2af72ffae16fb19219bef0e3
Andorryu <adority121@gmail.com>
2022-11-10 14:08:09 -0600

    wrote comments

---
+++ index.hs
@@ -0,0 +1,39 @@
+{-
+    EECS 368 Assignment 6
+    Defined the following 5 Haskell Functions:
+        - replicate
+        - perfects
+        - find
+        - positions
+        - scalarproduct
+-}
+
+{-
+    REPLICATE
+    INPUTS: a int representing the length of an array and a value
+    OUTPUTS: a list of size length where every element is value
+-}
+
+{-
+    PERFECTS
+    INPUTS: an integer n
+    OUTPUTS: a list of every perfect integer from 1 to n
+-}
+
+{-
+    FIND
+    INPUTS: an identifier and a list of key-value pairs
+    OUTPUTS: a list where each element is value if identifier == key
+-}
+
+{-
+    POSITIONS
+    INPUTS: an identifier and a list of values
+    OUTPUTS: a list of integers that holds the indexes where identifier == value
+-}
+
+{-
+    SCALARPRODUCT
+    INPUTS: two lists of integers
+    OUTPUTS: an int which is the scalar product of the two lists
+-}
