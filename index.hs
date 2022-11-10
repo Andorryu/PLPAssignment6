536dce0a1613ce38d65593c7f7d778ac6c18f1b5
Andorryu <adority121@gmail.com>
2022-11-10 14:17:24 -0600

    finished perfects

--- index.hs
+++ index.hs
@@ -21,6 +21,12 @@ replicate n v = [v | x <- [1..n]]
     INPUTS: an integer n
     OUTPUTS: a list of every perfect integer from 1 to n
 -}
+-- helper function - finds all factors of n up to n-1
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n-1], n `mod` x == 0]
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (factors x) == x]
+
 
 {-
     FIND
